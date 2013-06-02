4cff0ae1a1e1010fd638d02b2e8ce1c82bbf5885
badlogic <badlogicgames@gmail.com>
2013-06-02 14:04:57 +0200

    more examples

--- gdx-workshop/src/com/badlogic/gdx/workshop/files/InternalFileExample.java
+++ gdx-workshop/src/com/badlogic/gdx/workshop/files/InternalFileExample.java
@@ -4,6 +4,15 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 
+/**
+ * Demonstrates how to read the content of an internal text file.
+ * The file is located in the Android project's assets/data folder.
+ * The file path is specified relative to the assets/ folder, "data/test.txt",
+ * not "assets/data/test.txt".
+ * 
+ * @author badlogic
+ *
+ */
 public class InternalFileExample extends ApplicationAdapter {
 	@Override
 	public void create() {
--- gdx-workshop/src/com/badlogic/gdx/workshop/files/ReadWriteExample.java
+++ gdx-workshop/src/com/badlogic/gdx/workshop/files/ReadWriteExample.java
@@ -1,5 +1,35 @@
 package com.badlogic.gdx.workshop.files;
 
-public class ReadWriteExample {
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 
+/**
+ * Demonstrates how to read/write files. Files can
+ * only be written to local or external files. On
+ * Android, we also need to add the WRITE_EXTERNAL_STORAGE 
+ * permission to the AndroidManifest.xml file.
+ * @author badlogic
+ *
+ */
+public class ReadWriteExample extends ApplicationAdapter {
+
+	@Override
+	public void create() {
+		// Write a file to the local storage, overwrite it
+		// if it already exists. Then read it back in and
+		// output the content. Finally, delete the file
+		FileHandle local = Gdx.files.local("local-file.txt");
+		local.writeString("Local file!", false);
+		Gdx.app.log("ReadWriteExample", local.readString());
+		local.delete();
+		Gdx.app.log("ReadWriteExample", "Local file exists: " + local.exists());
+		
+		// Same as above, using an external file
+		FileHandle external = Gdx.files.local("external-file.txt");
+		external.writeString("External file!", false);
+		Gdx.app.log("ReadWriteExample", external.readString());
+		external.delete();
+		Gdx.app.log("ReadWriteExample", "External file exists: " + external.exists());
+	}
 }
